a303ba712e9125a839e125e6ef0528e11d718daa
evgeny kulakov <haintass@gmail.com>
2019-03-03 12:39:09 +0300

    Added second file reading logic

--- XmlWebApp/Controllers/LargeXmlController.cs
+++ XmlWebApp/Controllers/LargeXmlController.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using XmlWebApp.Models;
-using OfficeOpenXml;
 using System.Xml;
-using System.Xml.Schema;
 using System.Xml.Linq;
-using System.Xml.Serialization;
+
+using IOFile = System.IO.File;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace XmlWebApp.Controllers
 {
@@ -18,12 +17,56 @@ namespace XmlWebApp.Controllers
         public ActionResult GetBooks()
         {
 			var data = new List<Book>();
-
+			
 			data = GetData();
 
 			ViewBag.Books = data;
 
 			return View("LargeXml");
+		}
+
+		public List<Book> GetTextData() {
+			string filePath = Server.MapPath(Consts.bigDataListPath);
+			string newFilePath = Server.MapPath("~/XML/NewLargeBooksList.xml");
+
+			List<Book> books = new List<Book>();
+
+			using (StreamReader s = IOFile.OpenText(filePath)) {
+				Regex regex = new Regex(@"<book[\s||>]");
+				string str;
+				while ((str = s.ReadLine()) != null) {
+					if (regex.Match(str).Success) {
+						string xml = str + ReadOuterXml(s);
+
+						XmlDocument xmlDoc = new XmlDocument();
+						xmlDoc.LoadXml(xml);
+
+						XmlNode node = xmlDoc.SelectSingleNode("book");
+
+						books.Add(new Book {
+							Name = node.SelectSingleNode("name").InnerText,
+							Author = node.SelectSingleNode("author").InnerText,
+							Price = Convert.ToInt32(node.SelectSingleNode("price").InnerText),
+							Id = Convert.ToInt32(node.SelectSingleNode("id").InnerText)
+						});
+					}
+				}
+			}
+
+			return books;
+		}
+
+		private string ReadOuterXml(StreamReader s) {
+			Regex regex = new Regex(@"</book>");
+			string outerXml = string.Empty;
+
+			string str;
+			while (!regex.Match(str = s.ReadLine()).Success) {
+				outerXml += str;
+			}
+			outerXml += str;
+
+			return outerXml;
 		}
 
 		public List<Book> GetData() {
@@ -38,7 +81,7 @@ namespace XmlWebApp.Controllers
 					xmlDoc.LoadXml(xml.ReadOuterXml());
 
 					XmlNode node = xmlDoc.SelectSingleNode("book");
-
+					
 					books.Add(new Book {
 						Name = node.SelectSingleNode("name").InnerText,
 						Author = node.SelectSingleNode("author").InnerText,
@@ -54,65 +97,71 @@ namespace XmlWebApp.Controllers
 			return books;
 		}
 
-		public void CopyTo(XmlReader reader, XmlWriter writer, int id) {
-			while (reader.Read()) {
-				var currentElementName = "";
-				while (reader.Read()) {
-					//switch (reader.NodeType) {
-					//	case XmlNodeType.Element:
-					//		currentElementName = reader.Name;
-					//		writer.WriteStartElement(reader.Name);
+		public ActionResult Delete(int id) {
+			string filePath = Server.MapPath(Consts.bigDataListPath);
+			string newFilePath = Server.MapPath("~/XML/template.xml");
 
-					//		//Copy all attributes verbatim
-					//		if (reader.HasAttributes)
-					//			writer.WriteAttributes(reader, true);
+			using (XmlReader xml = XmlReader.Create(filePath)) {
+				using (XmlWriter writer = XmlWriter.Create(newFilePath)) {
+					writer.WriteStartDocument();
+					writer.WriteWhitespace("\n");
 
-					//		//Handle empty elements by telling the writer to close right away
-					//		if (reader.IsEmptyElement)
-					//			writer.WriteEndElement();
-					//		break;
-					//	case XmlNodeType.EndElement:
-					//		currentElementName = "";
-					//		writer.WriteEndElement();
-					//		break;
-					//	case XmlNodeType.Text:
-					//		if (reader.Value != id.ToString())
-					//		writer.WriteString(reader.Value);
-					//		break;
-					//	case XmlNodeType.Whitespace:
-					//		writer.WriteWhitespace(reader.Value);
-					//		break;
-					//		//Other cases. Attributes, comments etc.
-					//}
+					writer.WriteStartElement("books");
+					writer.WriteWhitespace("\n");
 
-					if (reader.NodeType == XmlNodeType.Element && reader.Name == "book") {
-						XmlDocument xmlDoc = new XmlDocument();
-						xmlDoc.LoadXml(reader.ReadOuterXml());
+					writer.WriteWhitespace("  ");
+					writer.WriteStartElement("bookCollection");
+					writer.WriteWhitespace("\n");
 
-						XmlNode node = xmlDoc.SelectSingleNode("book");
-						
-						string identifier = node.SelectSingleNode("id").InnerText;
+					while (xml.Read()) {
+						if (xml.NodeType == XmlNodeType.Element && xml.Name == "book") {
+							XmlDocument xmlDoc = new XmlDocument();
+							xmlDoc.LoadXml(xml.ReadOuterXml());
 
-						if (identifier != id.ToString()) {
-							writer.WriteNode(reader, true);
+							XmlNode node = xmlDoc.SelectSingleNode("book");
+
+							if (Convert.ToInt32(node.SelectSingleNode("id").InnerText) != id) {
+								writer.WriteWhitespace("    ");
+								writer.WriteStartElement("book");
+								writer.WriteAttributeString("language", node.Attributes.GetNamedItem("language").Value);
+								writer.WriteWhitespace("\n");
+
+								writer.WriteWhitespace("      ");
+								writer.WriteElementString("name", node.SelectSingleNode("name").InnerXml);
+								writer.WriteWhitespace("\n");
+
+								writer.WriteWhitespace("      ");
+								writer.WriteElementString("author", node.SelectSingleNode("author").InnerXml);
+								writer.WriteWhitespace("\n");
+
+								writer.WriteWhitespace("      ");
+								writer.WriteElementString("price", node.SelectSingleNode("price").InnerXml);
+								writer.WriteWhitespace("\n");
+
+								writer.WriteWhitespace("      ");
+								writer.WriteElementString("id", node.SelectSingleNode("id").InnerXml);
+								writer.WriteWhitespace("\n");
+
+								writer.WriteWhitespace("    ");
+								writer.WriteEndElement();
+								writer.WriteWhitespace("\n");
+							}
 						}
 					}
 
+					writer.WriteWhitespace("  ");
+					writer.WriteEndElement();
+					writer.WriteWhitespace("\n");
+
+					writer.WriteEndElement();
+					writer.WriteWhitespace("\n");
+
+					writer.WriteEndDocument();
 				}
 			}
-		}
-
-		public ActionResult Delete(int id) {
-			string filePath = Server.MapPath(Consts.bigDataListPath);
-
-			var xmlReader = XmlReader.Create(System.IO.File.OpenRead(filePath));
-			var xmlWriter = XmlWriter.Create(System.IO.File.OpenWrite(Server.MapPath("~/XML/NewLargeBooksList.xml")));
-
-
-			CopyTo(xmlReader, xmlWriter, id);
 			
-			xmlReader.Close();
-			xmlWriter.Close();
+			IOFile.Delete(filePath);
+			IOFile.Move(newFilePath, filePath);
 
 			return GetBooks();
 		}
@@ -120,7 +169,7 @@ namespace XmlWebApp.Controllers
 		public ActionResult DeleteAll() {
 			string filePath = Server.MapPath(Consts.bigDataListPath);
 
-			System.IO.File.Delete(filePath);
+			IOFile.Delete(filePath);
 
 			XDocument doc = new XDocument(new XElement("books", new XElement("bookCollection")));
 			doc.Save(filePath);
@@ -135,6 +184,7 @@ namespace XmlWebApp.Controllers
 			Random rand = new Random();
 			for (int i = 1; i <= nodesCount; i++) {
 				xDocument.Root.Element("bookCollection").Add(new XElement("book",
+							new XAttribute("language", rand.Next(1, 100) % 2 == 0 ? "English" : "Russian"),
 							new XElement("name", RandomString(rand, 10)),
 							new XElement("author", RandomString(rand, 10)),
 							new XElement("price", rand.Next(100, 10000)),
